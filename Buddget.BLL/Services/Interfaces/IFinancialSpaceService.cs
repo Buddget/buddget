0fec9eb3b413b462f0aea7a60a2e2d5f1d6a406c
Arsenii <mazur.arseniy@gmail.com>
2025-03-18 01:49:19 +0200

    Added ForgottenvView Financial Spaces Views and Models

--- Buddget.BLL/Services/Interfaces/IFinancialSpaceService.cs
+++ Buddget.BLL/Services/Interfaces/IFinancialSpaceService.cs
@@ -1,10 +1,12 @@
 ﻿using Buddget.BLL.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Buddget.BLL.Services.Interfaces
 {
     public interface IFinancialSpaceService
     {
         Task<FinancialSpaceDto> GetFinancialSpaceByIdAsync(int spaceId);
+        Task<IEnumerable<FinancialSpaceDto>> GetFinancialSpacesUserIsMemberOrOwnerOf(int userId);
     }
 }
-
