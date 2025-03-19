a14924841d7fc735ca29b52b855079fa2429b405
viktorpekhun <viktorpehun@gmail.com>
2025-03-19 12:38:55 +0200

    Change to financial space style and mapper

--- Buddget.BLL/Mappers/FinancialSpaceMemberProfile.cs
+++ Buddget.BLL/Mappers/FinancialSpaceMemberProfile.cs
@@ -12,7 +12,6 @@ public class FinancialSpaceMemberProfile : Profile
             .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.User.LastName))
             .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email)) // Ensure Email is mapped
             .ForMember(dest => dest.RegisteredAt, opt => opt.MapFrom(src => src.User.RegisteredAt))
-            .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role))
             .ForMember(dest => dest.FinancialSpaceId, opt => opt.MapFrom(src => src.FinancialSpaceId))
             .ForMember(dest => dest.MemberRole, opt => opt.MapFrom(src => src.Role));
     }
