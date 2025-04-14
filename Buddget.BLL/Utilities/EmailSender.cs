d8c874f96d3eceef0fa28fea33f3cee90354c9b2
nazariusPr <115506158+nazariusPr@users.noreply.github.com>
2025-04-15 01:28:09 +0300

    impl-email-sending (#50)

--- Buddget.BLL/Utilities/EmailSender.cs
+++ Buddget.BLL/Utilities/EmailSender.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using DotNetEnv;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Net;
+using System.Net.Mail;
 
 namespace Buddget.BLL.Utilities
 {
     public class EmailSender : IEmailSender
     {
+        private readonly string _smtpHost = "smtp.gmail.com";
+        private readonly int _smtpPort = 587;
+        private readonly string _smtpUser = Environment.GetEnvironmentVariable("SMTP_USER");
+        private readonly string _smtpPass = Environment.GetEnvironmentVariable("SMTP_PASS");
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            return Task.CompletedTask;
+            var fromAddress = new MailAddress(_smtpUser, "Budget App");
+            var toAddress = new MailAddress(email);
+
+            var smtp = new SmtpClient
+            {
+                Host = _smtpHost,
+                Port = _smtpPort,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(_smtpUser, _smtpPass),
+            };
+
+            var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = htmlMessage,
+                IsBodyHtml = true,
+            };
+
+            return smtp.SendMailAsync(message);
         }
     }
 }
